941b5245c217fb446ef4110f151fb75bb883ff86
Sean O'Connor <sean.f.oconnor4.civ@army.mil>
2024-08-13 13:36:56 -0500

    Add project files.

---
+++ BlazorDeepDive/Models/Server.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+
+namespace BlazorDeepDive.Models
+{
+    public class Server
+    {
+        public Server()
+        {
+            int randomNumber = new Random().Next(0, 2);
+            IsOnline = randomNumber == 0 ? false : true;
+        }
+
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? City {  get; set; }
+        public bool IsOnline { get; set; }
+
+    }
+}
